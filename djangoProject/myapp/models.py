4ec96abce724f9f95db9a2b3e69f3d2e31db790e
DavidCounagoGonzalez <dcounagogonzalez@danielcastelao.org>
2024-03-24 17:15:02 +0100

    Terminada la parte de admin

--- djangoProject/myapp/models.py
+++ djangoProject/myapp/models.py
@@ -4,7 +4,13 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title + ' - ' + self.project.name
